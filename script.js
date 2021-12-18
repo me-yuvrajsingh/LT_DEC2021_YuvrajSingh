772b88edeb88486ee8e8b46304f427c5d50b4c4e
me-yuvrajsingh <90550128+me-yuvrajsingh@users.noreply.github.com>
2021-12-18 20:47:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ let sideNav = document.querySelector('.android');
 let closeBtn = document.querySelector('.fa-times');
 let searchIcon = document.querySelector('.search-icon');
 let searchCont = document.querySelector('.search');
-let clearSearch = document.querySelector('.close')
+let clearSearch = document.querySelector('.close');
+
+var prevScrollpos = window.pageYOffset;
+
 
 
 
@@ -15,12 +18,20 @@ window.onscroll = ()=>{
   loginForm.classList.remove('active');
   searchCont.classList.remove('search-active');
 
+    var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.querySelector("#main-header").style.top = "0";
+      } else {
+        document.querySelector("#main-header").style.top = "-75px";
+      }
+      prevScrollpos = currentScrollPos;
+    
 }
 
 menuBar.addEventListener('click', ()=>{
   sideNav.classList.toggle('active-nav')
 
-  
+
 })
 
 
@@ -40,3 +51,17 @@ searchIcon.addEventListener('click',()=>{
 clearSearch.addEventListener('click', ()=>{
   searchCont.classList.toggle('search-active')
 })
+
+
+//if navbar sliding will not work properly then uncomment the below code: 
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.querySelector("#main-header").style.top = "0";
+//   } else {
+//     document.querySelector("#main-header").style.top = "-75px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
